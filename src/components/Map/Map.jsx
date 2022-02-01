9e9e0980d28f9c7489169929ba80c333935e121f
YagoFerr <yferreira@hsolmkt.com.br>
2022-02-01 16:19:05 -0300

    Building first components

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import googleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons';
+import Rating from '@material-ui/lab';
+
+import useStyles from './MapStyles';
+
+const Map = (props) => {
+  return (
+    <h1>Map</h1>
+  )
+};
+
+export default Map;
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlaceDetails = (props) => {
+  return (
+    <h1>Place Details</h1>
+  )
+};
+
+export default PlaceDetails;
